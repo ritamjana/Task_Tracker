44d5230a65683049d014b66d248c7fd277bb107d
Ritam Jana <ritamjana2002@gmail.com>
2021-04-29 21:56:48 +0530

    Completed basic UI. Start working on form next.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctor's Appointment",
+      day: "Feb 5th",
+      time: "2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th",
+      time: "10:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 6th",
+      time: "6:30pm",
+      reminder: false,
+    },
+  ]);
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks to Show"
+      )}
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Button from "./Button";
 
 const Header = () => {
+  const onClick = () => {
+    console.log("Click");
+  };
   return (
-    <header>
+    <header className="header">
       <h1>Task Tracker</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
   );
 };
